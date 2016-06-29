def08bfed68dee51fc040a83eb7b562bf98c01ac
Korirmitchelle <korirmitchelle@gmail.com>
2016-06-29 17:37:41 +0300

    slider

---
+++ js/mitch.js
@@ -0,0 +1,13 @@
+var blue = document.getElementById("slider");
+
+var orange = [];
+var counter = 0;
+
+function slidy(){
+    blue.style.backgroundImage = orange[counter];
+    counter++;
+
+    if (counter >= orange.length){
+        counter = 0
+    }
+}

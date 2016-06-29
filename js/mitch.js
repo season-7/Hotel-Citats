facdaa190e489dd9639b331eae8e01ad525d5c45
Korirmitchelle <korirmitchelle@gmail.com>
2016-06-29 19:25:49 +0300

    changes

--- js/mitch.js
+++ js/mitch.js
@@ -1,13 +1,19 @@
 var blue = document.getElementById("slider");
 
-var orange = [];
+var orange = [
+    "url('images/beeed.jpg')",
+    "url('images/conferencesector.jpg')",
+    "url('images/hotel-swimming-pool.jpg')",
+    "url('images/theoutsidesea.jpg')",
+    "url('images/livingroomsuite.jpg')"
+];
 var counter = 0;
 
-function slidy(){
+function slidy() {
     blue.style.backgroundImage = orange[counter];
     counter++;
 
-    if (counter >= orange.length){
+    if (counter >= orange.length) {
         counter = 0
     }
 }
